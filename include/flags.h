c60bd5b843e7622c49e681341f6601ed743bdb06
MarioS271 <102055042+MarioS271@users.noreply.github.com>
2025-10-09 14:55:30 +0200

    Add Flag And External Function Predef System

---
+++ include/flags.h
@@ -0,0 +1,14 @@
+// FLAGS_H
+// Boboter
+// (C) MarioS271 2025
+
+#pragma once
+
+// SHOW_DEBUG_LOGS
+//  Use to enable or disable debug logging
+constexpr bool SHOW_DEBUG_LOGS = false;
+
+// ENABLE_SENSOR_TEST_MODE
+//  This flag decides if the Robot will operate in "Sensor Test Mode",
+//  which tests the robot's sensors and functions
+constexpr bool ENABLE_SENSOR_TEST_MODE = false;
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "esp_random.h"
 #include "freertos/FreeRTOS.h"
 
+#include "flags.h"
+#include "main_function_defs.h"
 #include "delay.h"
 #include "rgb_color.h"
 #include "predef_colors.h"
@@ -17,12 +19,9 @@
 using namespace COLORS;
 
 static const char* TAG = "MAIN";
-constexpr bool ESP_SHOW_DEBUG_LOGS = true;  // Use to enable/disable debug logging
-
-void ledTask(void* pvParameters);
 
 extern "C" void app_main() {
-    esp_log_level_set("*", ESP_SHOW_DEBUG_LOGS ? ESP_LOG_DEBUG : ESP_LOG_INFO);
+    esp_log_level_set("*", SHOW_DEBUG_LOGS ? ESP_LOG_DEBUG : ESP_LOG_INFO);
     ESP_LOGI(TAG, "Hello, World!");
 
     Leds leds = Leds();
@@ -35,13 +34,14 @@ extern "C" void app_main() {
     
     // motorLeft.setDirection(FORWARD);
     // motorRight.setDirection(BACKWARD);
-    // motorLeft.setSpeed(700);
-    // motorRight.setSpeed(700);
+    // motorLeft.setSpeed(500);
+    // motorRight.setSpeed(500);
 
     while (true) {
         delay(5000);
     }
 }
+
 
 
 void ledTask(void* pvParameters) {
