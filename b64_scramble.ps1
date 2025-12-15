f22b613967a3e7ce4a17e4fa8bb1af56e4a67ae1
MarioS271 <102055042+MarioS271@users.noreply.github.com>
2025-12-15 17:50:55 +0100

    Remove PWSH helper script

--- b64_scramble.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$text
-)
-
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($text)
-$base64 = [System.Convert]::ToBase64String($bytes)
-Write-Output $base64
