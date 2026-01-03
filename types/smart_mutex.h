6343d8a6434f03474004e0eb2f7a633d09cce169
MarioS271 <marioerik.scholze@gmail.com>
2026-01-04 00:59:25 +0100

    Removed one dot from doxy comment cause it was bugging me >:(

--- types/smart_mutex.h
+++ types/smart_mutex.h
@@ -13,9 +13,9 @@
  * @brief A RAII wrapper for mutexes which locks them at the beginning of its lifetime and releases them
  *        at the end of its lifetime (normally the scope in which the wrapper was created)
  *
- * @details To create a smart mutex, just do: @code smart_mutex lock(mutex);@endcode. Done! Now,
- *          the mutex will stay locked until the function finishes (until this object goes out of scope and
- *          its destructor is called) and then it will automatically unlock the mutex for you.
+ * @details To create a smart mutex, just do: @code smart_mutex lock(mutex);@endcode
+ *          Done! Now, the mutex will stay locked until the function finishes (until this object goes out
+ *          of scope and its destructor is called) and then it will automatically unlock the mutex for you.
  *          Be careful that you don't lock the mutex and then call a function which again locks the mutex,
  *          which will cause the firmware to hang!!
  */
